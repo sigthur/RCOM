27e1fc1cf2a30b5372ddf5ba2c75b41fd9507244
MartimRRibas <163015407+MartimRRibas@users.noreply.github.com>
2025-10-06 10:32:59 +0100

    Update link_layer.c

--- lab1-code-25-26/src/link_layer.c
+++ lab1-code-25-26/src/link_layer.c
@@ -2,7 +2,18 @@
 
 #include "link_layer.h"
 #include "serial_port.h"
+#include <stdio.h>
 
+#define BAUDRATE 38400
+#define BUF_SIZE 5
+unsigned char flag = 0x7E;
+unsigned char A = 0x01;
+unsigned char C = 0x07;
+unsigned char BCC1 = 0x06;
+unsigned char add = 0x03;
+unsigned char c = 0x03;
+unsigned char bcc = 0x00;
+volatile int STOP = FALSE;
 // MISC
 #define _POSIX_SOURCE 1 // POSIX compliant source
 
@@ -24,7 +35,7 @@ int llopen(LinkLayer connectionParameters)
     // TODO: Implement this function
     int nBytesBuf = 0;
     int currentState = 0;
-    if (connectionParameters.role = LlRx) {
+    if (connectionParameters.role == LlRx) {
         while (STOP == FALSE)
         {   
         // Read one byte from serial port.
@@ -79,6 +90,7 @@ int llopen(LinkLayer connectionParameters)
 
         }
         }
+        printf("Total bytes received: %d\n", nBytesBuf);
         unsigned char buf[BUF_SIZE] = {0};
         buf[0] = flag;
         buf[1] = A;
@@ -86,8 +98,9 @@ int llopen(LinkLayer connectionParameters)
         buf[3] = BCC1;
         buf[4] = flag;
         int bytes = writeBytesSerialPort(buf, 5);
+        printf("%d bytes written to serial port\n", bytes);
 
-        printf("Total bytes received: %d\n", nBytesBuf);
+        
 
     }
     return 0;
