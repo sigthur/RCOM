d6913544877a0c031ff8e4cbf4747425a872df34
MartimRRibas <163015407+MartimRRibas@users.noreply.github.com>
2025-10-06 10:31:17 +0100

    corrected application
    Added serialPort to connection parameters initialization.

--- lab1-code-25-26/src/application_layer.c
+++ lab1-code-25-26/src/application_layer.c
@@ -2,7 +2,6 @@
 
 #include "application_layer.h"
 #include "link_layer.h"
-
 #include <stdio.h>
 #include <string.h>
 
@@ -10,11 +9,11 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
     // TODO: Implement this function
-  LinkLayer connectionParameters;
-  connectionParameters.role = strcmp(role, "tx") ? LlRx : LlTx;
-  connectionParameters.baudRate = baudRate;
-  connectionParameters.nRetransmissions = nTries;
-  connectionParameters.timeout = timeout;
-
-  llopen(connectionParameters);
+    LinkLayer connectionParameters;
+    strcpy(connectionParameters.serialPort,serialPort);
+    connectionParameters.role = strcmp(role, "tx") ? LlRx : LlTx;
+    connectionParameters.baudRate = baudRate;
+    connectionParameters.nRetransmissions = nTries;
+    connectionParameters.timeout = timeout;
+    //llopen(connectionParameters);
 }
