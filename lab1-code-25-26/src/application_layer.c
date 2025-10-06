81067cfbfc225f05d455b492a54047a1eac0e904
MartimRRibas <163015407+MartimRRibas@users.noreply.github.com>
2025-10-06 10:04:21 +0100

    Initialize link layer parameters in application layer
    Added link layer initialization in application layer.

--- lab1-code-25-26/src/application_layer.c
+++ lab1-code-25-26/src/application_layer.c
@@ -1,11 +1,20 @@
 // Application layer protocol implementation
 
 #include "application_layer.h"
+#include "link_layer.h"
 
 #include <stdio.h>
+#include <string.h>
 
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
     // TODO: Implement this function
+  LinkLayer connectionParameters;
+  connectionParameters.role = strcmp(role, "tx") ? LlRx : LlTx;
+  connectionParameters.baudRate = baudRate;
+  connectionParameters.nRetransmissions = nTries;
+  connectionParameters.timeout = timeout;
+
+  llopen(connectionParameters);
 }
